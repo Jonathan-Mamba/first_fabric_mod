a6e654bb3f76de3486da58188daafcfb134227dc
Jonathan-Mamba <jojodiallo.jd@gmail.com>
2024-02-03 22:03:30 +0100

    Ajout du code pour un bloc de stylos mais ca marche pas

--- src/main/java/name/modid/TutorialMod.java
+++ src/main/java/name/modid/TutorialMod.java
@@ -1,18 +1,21 @@
 package name.modid;
 
+import name.modid.block.ModBlocks;
 import name.modid.item.ModItemGroups;
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.item.ItemGroup;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
-	public static final String MOD_ID = "tutorial-mod";
+	public static final String MOD_ID = "tutorialmod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Hello Fabric world!");
 		ModItemGroups.registerItemsGroup();
+		boolean var = ModItemGroups.MOD_ITEMS_GROUP.getRow().toString() == null;
 	}
 }
--- src/main/java/name/modid/item/ModItemGroups.java
+++ src/main/java/name/modid/item/ModItemGroups.java
@@ -1,26 +1,27 @@
 package name.modid.item;
 
-import name.modid.TutorialMod;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import name.modid.TutorialMod;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
+
 public class ModItemGroups {
     public static final ItemGroup MOD_ITEMS_GROUP = Registry.register(Registries.ITEM_GROUP,
-            new Identifier(TutorialMod.MOD_ID, "jsp"),
+            new Identifier(TutorialMod.MOD_ID, "mod"),
             FabricItemGroup.builder().displayName(Text.translatable("itemgroup.tutorialmod"))
                     .icon(() -> new ItemStack(ModItems.BIC_CTYSTAL)).entries((displayContext, entries) -> {
                         entries.add(ModItems.BIC_CTYSTAL);
                         entries.add(ModItems.STYLO_NWAR);
+                        // entries.add(ModBlocks.STYLO_BLOCK);
                     }).build());
-
 
     public static void registerItemsGroup() {
         TutorialMod.LOGGER.info("Registering item groups");
-
     }
 }
