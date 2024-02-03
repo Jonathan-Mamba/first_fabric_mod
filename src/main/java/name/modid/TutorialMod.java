fecc73b13932657ed8904d568c29d1df0ec2ffaf
Jonathan-Mamba <jojodiallo.jd@gmail.com>
2024-02-03 13:11:09 +0100

    apparament il manque des fichiers je vais voir

---
+++ src/main/java/name/modid/TutorialMod.java
@@ -0,0 +1,22 @@
+package name.modid;
+
+import name.modid.item.ModItems;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorial_mod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		for (int i = 0; i < 20; i++) {
+			LOGGER.info("Hello Fabric world!");
+		}
+		ModItems.registerModItems();
+
+	}
+}
---
+++ src/main/java/name/modid/item/ModItems.java
@@ -0,0 +1,9 @@
+package name.modid.item;
+
+import name.modid.TutorialMod;
+
+public class ModItems {
+    public static void registerModItems() {
+        TutorialMod.LOGGER.info("Registering items for " + TutorialMod.MOD_ID);
+    }
+}
